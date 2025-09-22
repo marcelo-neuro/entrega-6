a0d874db9e23dffef4aa7d93137f4b34dfd7e363
marcelo-neuro <marcelofurla0813@gmail.com>
2025-09-22 18:23:41 -0300

    Feat: Cartao entity created

---
+++ src/main/java/com/mindmatch/pagamento/entities/Cartao.java
@@ -0,0 +1,31 @@
+package com.mindmatch.pagamento.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+@Entity
+@Table(name = "tb_cartao")
+public class Cartao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 20, nullable = false)
+    private String numero;
+    @Column(length = 3, nullable = false)
+    private String cvv;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoCartao tipoCartao;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", nullable = false)
+    private Cliente cliente;
+}
---
+++ src/main/java/com/mindmatch/pagamento/entities/TipoCartao.java
@@ -0,0 +1,6 @@
+package com.mindmatch.pagamento.entities;
+
+public enum TipoCartao {
+    DEBITO,
+    CREDITO
+}
