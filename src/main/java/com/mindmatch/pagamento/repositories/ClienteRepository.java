0cd037d993a5e0951cfb4c70cc46864563b928e6
marcelo-neuro <marcelofurla0813@gmail.com>
2025-09-22 16:38:15 -0300

    Feat: Created ClienteRepository

---
+++ src/main/java/com/mindmatch/pagamento/repositories/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.mindmatch.pagamento.repositories;
+
+import com.mindmatch.pagamento.entities.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
