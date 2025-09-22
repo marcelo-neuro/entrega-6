8961dc0733d9fe8ff703f00554cc0178ac43b500
marcelo-neuro <marcelofurla0813@gmail.com>
2025-09-22 18:58:01 -0300

    Feat: CartaoDTO created

---
+++ src/main/java/com/mindmatch/pagamento/dto/CartaoDTO.java
@@ -0,0 +1,39 @@
+package com.mindmatch.pagamento.dto;
+
+import com.mindmatch.pagamento.entities.Cartao;
+import com.mindmatch.pagamento.entities.TipoCartao;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class CartaoDTO {
+
+    private Long id;
+    @Size(min = 16 , max = 20, message = "O numero do cartão deve conter entre 16 e 20 caracteres.")
+    @NotBlank(message = "O campo \"numero\" é obrigatório.")
+    private String numero;
+    @Size(min = 16 , max = 20, message = "O numero do cartão deve conter entre 16 e 20 caracteres.")
+    @NotBlank(message = "O campo \"numero\" é obrigatório.")
+    private String cvv;
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "O campo \"numero\" é obrigatório.")
+    private TipoCartao tipoCartao;
+    @NotNull(message = "O campo \"clienteDto\" é obrigatório.")
+    private ClienteDTO clienteDto;
+
+    public CartaoDTO(Cartao entity) {
+        this.id = entity.getId();
+        this.numero = entity.getNumero();
+        this.cvv = entity.getCvv();
+        this.tipoCartao = entity.getTipoCartao();
+        this.clienteDto = new ClienteDTO(entity.getCliente());
+    }
+}
--- src/main/java/com/mindmatch/pagamento/dto/ClienteDTO.java
+++ src/main/java/com/mindmatch/pagamento/dto/ClienteDTO.java
@@ -22,8 +22,10 @@ public class ClienteDTO {
     private String nome;
     @Email(message = "O e-mail do cliente precisa ser válido.")
     @Size(min = 3 , max = 200, message = "O e-mail do cliente deve conter entre 3 e 200 caracteres.")
+    @NotBlank(message = "O campo \"email\" é obrigatório.")
     private String email;
     @Size(min = 9, max = 14, message = "O telefone do cliente deve conter entre 9 e 14 caracteres.")
+    @NotBlank(message = "O campo \"telefone\" é obrigatório.")
     private String telefone;
     @Positive(message = "O campo deve ser positivo.")
     private Double valorMedioCompra;
