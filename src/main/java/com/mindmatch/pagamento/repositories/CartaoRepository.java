08945f85ff9d45c1b9690985b8b5f59af1cce9d8
marcelo-neuro <marcelofurla0813@gmail.com>
2025-09-22 19:00:58 -0300

    Feat: CartaoRepository created

---
+++ src/main/java/com/mindmatch/pagamento/repositories/CartaoRepository.java
@@ -0,0 +1,9 @@
+package com.mindmatch.pagamento.repositories;
+
+import com.mindmatch.pagamento.entities.Cartao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartaoRepository extends JpaRepository<Cartao, Long> {
+}
